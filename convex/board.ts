be10b43e4666093bfc5947c964e89cc035049d0e
patolord <rodrigo_luizp@hotmail.com>
2024-03-09 22:57:09 -0300

    Update liveblocks.config.ts with authEndpoint

--- convex/board.ts
+++ convex/board.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 const images = [
   //list all images on public/placeholders folder
@@ -152,6 +152,17 @@ export const unfavorite = mutation({
     }
 
     await ctx.db.delete(existingFavorite._id);
+    return board;
+  },
+});
+
+export const get = query({
+  args: {
+    id: v.id("boards"),
+  },
+  handler: async (ctx, args) => {
+    const board = await ctx.db.get(args.id);
+    if (!board) throw new Error("Board not found");
     return board;
   },
 });
--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -2,8 +2,7 @@ import { createClient } from "@liveblocks/client";
 import { createRoomContext, createLiveblocksContext } from "@liveblocks/react";
 
 const client = createClient({
-  publicApiKey:
-    "pk_dev_eAdeDF_3IrtbHcV4yv7vAqXNBMDu18ussUPtJPBjk4uT4hd4GNgQLN1VOamlUwx0",
+  authEndpoint: "/api/liveblocks-auth",
 
   async resolveUsers({ userIds }) {
     // Used only for Comments and Notifications. Return a list of user information
@@ -68,6 +67,12 @@ type Storage = {
 // provided by your own custom auth back end (if used). Useful for data that
 // will not change during a session, like a user's name or avatar.
 type UserMeta = {
+  id?: string;
+  info?: {
+    name?: string;
+    picture?: string;
+  };
+
   // id?: string,  // Accessible through `user.id`
   // info?: Json,  // Accessible through `user.info`
 };
